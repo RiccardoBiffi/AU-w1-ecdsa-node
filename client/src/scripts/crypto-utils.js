6e0deb3062bcb535b16a7adb278ee18f7c54a0f5
Riccardo Biffi <biffi.riccardo91@gmail.com>
2023-07-01 14:52:14 +0200

    Update crypto utils

--- client/src/scripts/crypto-utils.js
+++ client/src/scripts/crypto-utils.js
@@ -1,14 +1,18 @@
 import { keccak256 } from "ethereum-cryptography/keccak";
 import { utf8ToBytes } from "ethereum-cryptography/utils";
-import { sign } from "ethereum-cryptography/secp256k1";
+import { secp256k1 } from "ethereum-cryptography/secp256k1";
+import { toHex } from "ethereum-cryptography/utils";
 
-export function hashMessage(message) {
-    const bytes = utf8ToBytes(message)
-    return keccak256(bytes)
+
+export function getAddress(publicKey) {
+    return "0x" + toHex(keccak256(publicKey.slice(1)).slice(-20));
 }
 
-export async function signMessage(msg, privateKey) {
-    console.log(msg, privateKey)
-    const hasedMsg = hashMessage(msg)
-    return sign(hasedMsg, privateKey, { recovered: true })
+export function hashMessage(message) {
+    return keccak256(utf8ToBytes(message))
+}
+
+export function signMessage(msg, privateKey) {
+    const signedMsg = secp256k1.sign(hashMessage(msg), privateKey)
+    return [signedMsg.toCompactHex(), signedMsg.recovery]
 }
